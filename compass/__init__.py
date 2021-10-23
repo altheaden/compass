a7fcd4608b8108d3c51a1f5bdaeda0dce5b827fc
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-10-23 15:45:16 -0600

    Add Substep to top-level imports

--- compass/__init__.py
+++ compass/__init__.py
@@ -2,3 +2,4 @@ from compass.mpas_core import MpasCore
 from compass.testgroup import TestGroup
 from compass.testcase import TestCase
 from compass.step import Step
+from compass.substep import Substep
