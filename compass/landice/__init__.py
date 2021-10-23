8ff1c1f7f30b410bddf1c4a9b2d98ad9f78d0d74
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-10-23 16:04:32 -0600

    TEMP COMMIT: comment out most test groups

--- compass/landice/__init__.py
+++ compass/landice/__init__.py
@@ -1,17 +1,17 @@
 from compass.mpas_core import MpasCore
-from compass.landice.tests.antarctica import Antarctica
-from compass.landice.tests.calving_dt_convergence import CalvingDtConvergence
+# from compass.landice.tests.antarctica import Antarctica
+# from compass.landice.tests.calving_dt_convergence import CalvingDtConvergence
 from compass.landice.tests.circular_shelf import CircularShelf
 from compass.landice.tests.dome import Dome
-from compass.landice.tests.eismint2 import Eismint2
-from compass.landice.tests.enthalpy_benchmark import EnthalpyBenchmark
-from compass.landice.tests.greenland import Greenland
-from compass.landice.tests.humboldt import Humboldt
-from compass.landice.tests.hydro_radial import HydroRadial
-from compass.landice.tests.kangerlussuaq import Kangerlussuaq
-from compass.landice.tests.koge_bugt_s import KogeBugtS
-from compass.landice.tests.mismipplus import MISMIPplus
-from compass.landice.tests.thwaites import Thwaites
+# from compass.landice.tests.eismint2 import Eismint2
+# from compass.landice.tests.enthalpy_benchmark import EnthalpyBenchmark
+# from compass.landice.tests.greenland import Greenland
+# from compass.landice.tests.humboldt import Humboldt
+# from compass.landice.tests.hydro_radial import HydroRadial
+# from compass.landice.tests.kangerlussuaq import Kangerlussuaq
+# from compass.landice.tests.koge_bugt_s import KogeBugtS
+# from compass.landice.tests.mismipplus import MISMIPplus
+# from compass.landice.tests.thwaites import Thwaites
 
 
 class Landice(MpasCore):
@@ -25,16 +25,16 @@ class Landice(MpasCore):
         """
         super().__init__(name='landice')
 
-        self.add_test_group(Antarctica(mpas_core=self))
-        self.add_test_group(CalvingDtConvergence(mpas_core=self))
+        # self.add_test_group(Antarctica(mpas_core=self))
+        # self.add_test_group(CalvingDtConvergence(mpas_core=self))
         self.add_test_group(CircularShelf(mpas_core=self))
         self.add_test_group(Dome(mpas_core=self))
-        self.add_test_group(Eismint2(mpas_core=self))
-        self.add_test_group(EnthalpyBenchmark(mpas_core=self))
-        self.add_test_group(Greenland(mpas_core=self))
-        self.add_test_group(Humboldt(mpas_core=self))
-        self.add_test_group(HydroRadial(mpas_core=self))
-        self.add_test_group(Kangerlussuaq(mpas_core=self))
-        self.add_test_group(KogeBugtS(mpas_core=self))
-        self.add_test_group(MISMIPplus(mpas_core=self))
-        self.add_test_group(Thwaites(mpas_core=self))
+        # self.add_test_group(Eismint2(mpas_core=self))
+        # self.add_test_group(EnthalpyBenchmark(mpas_core=self))
+        # self.add_test_group(Greenland(mpas_core=self))
+        # self.add_test_group(Humboldt(mpas_core=self))
+        # self.add_test_group(HydroRadial(mpas_core=self))
+        # self.add_test_group(Kangerlussuaq(mpas_core=self))
+        # self.add_test_group(KogeBugtS(mpas_core=self))
+        # self.add_test_group(MISMIPplus(mpas_core=self))
+        # self.add_test_group(Thwaites(mpas_core=self))
--- compass/ocean/__init__.py
+++ compass/ocean/__init__.py
@@ -1,18 +1,18 @@
 from compass.mpas_core import MpasCore
 from compass.ocean.tests.baroclinic_channel import BaroclinicChannel
-from compass.ocean.tests.drying_slope import DryingSlope
+# from compass.ocean.tests.drying_slope import DryingSlope
 from compass.ocean.tests.global_convergence import GlobalConvergence
-from compass.ocean.tests.global_ocean import GlobalOcean
-from compass.ocean.tests.gotm import Gotm
-from compass.ocean.tests.internal_wave import InternalWave
-from compass.ocean.tests.ice_shelf_2d import IceShelf2d
+# from compass.ocean.tests.global_ocean import GlobalOcean
+# from compass.ocean.tests.gotm import Gotm
+# from compass.ocean.tests.internal_wave import InternalWave
+# from compass.ocean.tests.ice_shelf_2d import IceShelf2d
 from compass.ocean.tests.isomip_plus import IsomipPlus
-from compass.ocean.tests.planar_convergence import PlanarConvergence
-from compass.ocean.tests.soma import Soma
-from compass.ocean.tests.sphere_transport import SphereTransport
-from compass.ocean.tests.spherical_harmonic_transform import \
-    SphericalHarmonicTransform
-from compass.ocean.tests.ziso import Ziso
+# from compass.ocean.tests.planar_convergence import PlanarConvergence
+# from compass.ocean.tests.soma import Soma
+# from compass.ocean.tests.sphere_transport import SphereTransport
+# from compass.ocean.tests.spherical_harmonic_transform import \
+#     SphericalHarmonicTransform
+# from compass.ocean.tests.ziso import Ziso
 
 
 class Ocean(MpasCore):
@@ -27,15 +27,15 @@ class Ocean(MpasCore):
         super().__init__(name='ocean')
 
         self.add_test_group(BaroclinicChannel(mpas_core=self))
-        self.add_test_group(DryingSlope(mpas_core=self))
+        # self.add_test_group(DryingSlope(mpas_core=self))
         self.add_test_group(GlobalConvergence(mpas_core=self))
-        self.add_test_group(GlobalOcean(mpas_core=self))
-        self.add_test_group(Gotm(mpas_core=self))
-        self.add_test_group(InternalWave(mpas_core=self))
-        self.add_test_group(IceShelf2d(mpas_core=self))
+        # self.add_test_group(GlobalOcean(mpas_core=self))
+        # self.add_test_group(Gotm(mpas_core=self))
+        # self.add_test_group(InternalWave(mpas_core=self))
+        # self.add_test_group(IceShelf2d(mpas_core=self))
         self.add_test_group(IsomipPlus(mpas_core=self))
-        self.add_test_group(PlanarConvergence(mpas_core=self))
-        self.add_test_group(Soma(mpas_core=self))
-        self.add_test_group(SphereTransport(mpas_core=self))
-        self.add_test_group(SphericalHarmonicTransform(mpas_core=self))
-        self.add_test_group(Ziso(mpas_core=self))
+        # self.add_test_group(PlanarConvergence(mpas_core=self))
+        # self.add_test_group(Soma(mpas_core=self))
+        # self.add_test_group(SphereTransport(mpas_core=self))
+        # self.add_test_group(SphericalHarmonicTransform(mpas_core=self))
+        # self.add_test_group(Ziso(mpas_core=self))
